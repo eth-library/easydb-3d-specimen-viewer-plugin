b15ef02f34c8aa5bdbfdad65fa79895eb0d206e9
Tim Bernhard <tim@bernhard-webstudio.ch>
2024-11-08 19:59:47 +0100

    Update dependencies, adjust style appropriately

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App specimen-3d-viewer-full-size">
       <SpecimenViewer3D src={asset} />
     </div>
   );
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import { Metric } from 'web-vitals';
+
+interface ReportHandler {
+  (entry: Metric): void;
+}
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
